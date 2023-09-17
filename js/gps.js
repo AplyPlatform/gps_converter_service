c6e3325d3a0dd1c642d1811ee3393e9c563f602a
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-09-17 19:24:25 +0900

    aply 서비스 링크 목록 - 공통 스크립트 사용하게끔 개선

--- js/gps.js
+++ js/gps.js
@@ -11,25 +11,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const targetsInfo = [
-	["aply", "https://aply.biz/"],
-	["aqr", "https://aqr.aplx.link"],
-	["recruit", "https://home.aply.biz/recruit/index.html"],
-	["duni", "https://duni.io"],
-	["dunipilot", "https://pilot.duni.io"],
-	["dunistock", "https://dunistock.com"],
-	["dromi", "https://dromi.aply.biz"],
-	["aplx", "https://home.aplx.link"],
-	["dkdk", "https://dkdk.io"],
-	["drdr", "https://drdr.io"],
-	["blog", "https://blog.naver.com/PostList.naver?blogId=jebo78&categoryNo=38&from=postList"],
-	["catchme", "https://catchme.aply.biz"],
-	["kiosk", "https://kiosk.aply.biz"],
-	["polaris", "https://polarisconsulting.modoo.at"],
-	["wifi", "https://wifi.aply.biz"],
-	["store", "https://smartstore.naver.com/aqr"]
-];
-
 
 let isRecaptchaInit = false;
 
@@ -89,21 +70,6 @@ function utilInit() {
 				GATAGM("util_lat_lng_input_key_enter", "CONTENT");
 				requestAddress();  //
 		}
-	});
-
-	
-	$('#selSites').change(function () {
-		let targetVal = $(this).val();
-		let targetUrl = "";
-
-		targetsInfo.forEach(function (t) {
-			if (t[0] == targetVal) {
-				targetUrl = t[1];
-			}
-		});
-
-		if (targetUrl != "")
-			window.open(targetUrl, "_blank");
 	});
 
 	setSubmitHandler("email_up");
