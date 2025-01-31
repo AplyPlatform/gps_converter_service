e6b9de35acbb8e8d100298af3d35bde8c9aa2be0
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2025-01-31 13:45:52 +0900

    aapi 적용

--- js/gps.js
+++ js/gps.js
@@ -12,13 +12,8 @@ limitations under the License.
 */
 
 
-let isRecaptchaInit = false;
-
 $(function () {
-		utilInit();
-		turnstile.ready(function () {
-			isRecaptchaInit = true;			
-		});
+		utilInit();		
 });
 
 var goToTop = function() {
@@ -60,46 +55,29 @@ function utilInit() {
 	
 	$("#address").keypress(function (e) {
 		if (e.which == 13){
-				GATAGM("util_address_input_key_enter", "CONTENT");
+				AAPI_GA_EVENT("util_address_input_key_enter", "GPS", "");
 				requestGPS();  //
 		}
 	});
 
 	$("#lng").keypress(function (e) {
 		if (e.which == 13){
-				GATAGM("util_lat_lng_input_key_enter", "CONTENT");
+				AAPI_GA_EVENT("util_lat_lng_input_key_enter", "GPS", "");
 				requestAddress();  //
 		}
-	});
+	});	
 
-	setSubmitHandler("email_up");
+	sendAAPIContactFormData("gpscontact");
 
 	goToTop();
 	hideLoader();
 }
 
 function setCaptcha(fd, successHandler, failHandler) {
-	if (isRecaptchaInit == false) {
-		turnstile.ready(function () {
-			isRecaptchaInit = true;
-			turnstile.render('#turnstileWidget', {
-				sitekey: '0x4AAAAAAA62_43H2MO9goDN',
-				callback: function(token) {
-					fd.append("form_token", token);
-					ajaxRequest(fd, successHandler, failHandler);
-				},
-			});
-		});
-	}
-	else {
-		turnstile.render('#turnstileWidget', {
-			sitekey: '0x4AAAAAAA62_43H2MO9goDN',
-			callback: function(token) {
-				fd.append("form_token", token);
-				ajaxRequest(fd, successHandler, failHandler);
-			},
-		});
-	}
+	AAPI_getCaptchaToken(function(token) {
+		fd.append("form_token", token);
+		ajaxRequest(fd, successHandler, failHandler);
+	});
 }
 
 var oldAddressVal = "";
@@ -110,9 +88,9 @@ function requestAddress() {
 	let fd = new FormData();
 	fd.append("action", "address_by_gps");
 	
-    if (isSet($("#lat").val()) == false || isSet($("#lng").val()) == false) {
+    if (AAPI_isSet($("#lat").val()) == false || AAPI_isSet($("#lng").val()) == false) {
 		hideLoader();
-		showDialog("올바른 좌표를 입력해 주세요", null);
+		AAPI_showDialog("올바른 좌표를 입력해 주세요");
 		return;
     }
 
@@ -128,24 +106,24 @@ function requestAddress() {
 	oldLatVal = $("#lat").val() * 1;
 	oldLngVal = $("#lng").val() * 1;
 
-	GATAGM("util_address_by_gps_btn_click", "SERVICE", oldLatVal + "," + oldLngVal);
+	AAPI_GA_EVENT("util_address_by_gps_btn_click", "GPS", oldLatVal + "," + oldLngVal);
 
 	showLoader();
 	setCaptcha(fd, function (r) {
 		if(r.result == "success") {
 			$("#address").val(r.data.address);
 			oldAddressVal = r.data.address;
-			showDialog("좌표를 주소로 변환 하였습니다.", null);
+			AAPI_showDialog("좌표를 주소로 변환 하였습니다.");
 			hideLoader();				
 		}
 		else {
-			showDialog("좌표의 변환에 오류가 발생했습니다. 다시 시도해 주세요", null);
+			AAPI_showDialog("좌표의 변환에 오류가 발생했습니다. 다시 시도해 주세요");
 			hideLoader();
 		}
 		},
 		function(request,status,error) {
 			hideLoader();
-			showDialog("좌표의 변환에 오류가 발생했습니다. 다시 시도해 주세요", null);
+			AAPI_showDialog("좌표의 변환에 오류가 발생했습니다. 다시 시도해 주세요");
 		}
 	);
 
@@ -153,8 +131,8 @@ function requestAddress() {
 
 function requestGPS() {
 	
-    if (isSet($("#address").val()) == false) {
-		showDialog("올바른 주소를 입력해 주세요.", null);
+    if (AAPI_isSet($("#address").val()) == false) {
+		AAPI_showDialog("올바른 주소를 입력해 주세요.");
 	    return;
     }
 
@@ -164,19 +142,19 @@ function requestGPS() {
 
     //같은 값으로 조회 시도
 	if (oldAddressVal == $("#address").val()) {
-		showDialog("이전에 조회한 주소와 동일한 주소입니다.", null);	    
+		AAPI_showDialog("이전에 조회한 주소와 동일한 주소입니다.");
 		return;
 	}
 
 	oldAddressVal = $("#address").val();
 
-	GATAGM("util_gps_by_address_btn_click", "SERVICE", oldAddressVal);
+	AAPI_GA_EVENT("util_gps_by_address_btn_click", "GPS", oldAddressVal);
 
 	showLoader();
 	setCaptcha(fd, function (r) {
 		if(r.result == "success") {
 				if (r.data == null) {
-					showDialog("주소를 변환하는데 실패하였습니다", null);
+					AAPI_showDialog("주소를 변환하는데 실패하였습니다");
 					return;
 				}
 										
@@ -186,176 +164,19 @@ function requestGPS() {
 					
 				oldAddressVal = r.data.address;
 				
-				showDialog("주소를 좌표로 변환 하였습니다.", null);
+				AAPI_showDialog("주소를 좌표로 변환 하였습니다.");
 				hideLoader();					    					    					  
 		}
 		else {
 				hideLoader();
-				showDialog("주소를 잘 못 입력하셨습니다", null);
+				AAPI_showDialog("주소를 잘 못 입력하셨습니다");
 		}
 	},
 	function(request,status,error) {
 		hideLoader();
-		showDialog("일시적인 오류가 발생하였습니다.", null);
+		AAPI_showDialog("일시적인 오류가 발생하였습니다.");
 	}
 	);
-}
-
-var appSent = false;
-function sendApplicationData(form_id, token)
-{
-	let min_type = "";
-	if ($(form_id).find('input[name="min_type_1"]').is(":checked")) {
-		min_type = "/서비스문의";
-	}
-
-	if ($(form_id).find('input[name="min_type_2"]').is(":checked")) {
-		min_type = min_type + "/제휴및협업";
-	}
-
-	if ($(form_id).find('input[name="min_type_3"]').is(":checked")) {
-		min_type = min_type + "/SW개발";
-	}
-
-	if ($(form_id).find('input[name="min_type_4"]').is(":checked")) {
-		min_type = min_type + "/기타문의";
-	}
-
-	if (min_type == "") {
-		showDialog("문의 종류를 선택해 주세요.", null);
-		hideLoader();
-		return false;
-	}
-
-	let form_content = $("#form_content").val();
-	if (form_content == "") {
-		showDialog("문의 내용을 입력해 주세요.", null);
-		hideLoader();
-		return false;
-	}
-
-	let form_phone = $(form_id).find('input[name="form_phone"]').val();
-	if (form_phone == "") {
-		showDialog("전화번호를 입력해 주세요.", null);
-		hideLoader();
-		return false;
-	}
-
-	let form_email = $(form_id).find('input[name="form_email"]').val();
-	if (form_email == "") {
-		showDialog("이메일을 입력해 주세요.", null);
-		hideLoader();
-		return false;
-	}
-
-	if ($(form_id).find("#agree_1").length > 0 && $(form_id).find("#agree_1").is(":checked") == false) {
-		showDialog("개인정보 처리방침에 동의해주세요.", null);
-		hideLoader();
-		return false;
-	}	
-	
-	let ref = $('<input type="hidden" value="' + document.referrer + '" name="ref">');	
-	$(form_id).append(ref);	
-	ref = $('<input type="hidden" value="' + min_type + '" name="min_type">');	
-	$(form_id).append(ref);	
-	ref = $('<input type="hidden" value="gpscontact" name="form_kind">');	
-	$(form_id).append(ref);
-
-	if (isRecaptchaInit == false) {
-		turnstile.ready(function () {
-			isRecaptchaInit = true;
-			turnstile.render('#turnstileWidget', {
-				sitekey: '0x4AAAAAAA62_43H2MO9goDN',
-				callback: function(token) {
-					$(form_id).find('input[name="form_token"]').val(token);
-					let fed = new FormData($(form_id)[0]);
-					ajaxRequestForContact(form_id, fed);
-				},
-			});
-		});
-	}
-	else {
-		turnstile.render('#turnstileWidget', {
-			sitekey: '0x4AAAAAAA62_43H2MO9goDN',
-			callback: function(token) {
-				$(form_id).find('input[name="form_token"]').val(token);
-				let fed = new FormData($(form_id)[0]);
-				ajaxRequestForContact(form_id, fed);
-			},
-		});
-	}
-	
-}
-
-function ajaxRequestForContact(form_id, fed) {
-	$.ajax({
-		type: "POST",
-		url: 'https://aply.biz/contact/handler.php',
-		crossDomain: true,
-		dataType: "json",
-		data:fed,
-		enctype: 'multipart/form-data', // 필수
-		processData: false,
-		contentType: false,
-		cache: false,
-		success: function (data) {
-			hideLoader();
-			if (data.result == "success") {
-				$(form_id + " input").last().remove();
-				showDialog("전송이 완료되었습니다. APLY가 연락 드리겠습니다.", function() {
-					location.href="/index.html";
-				});
-				return;
-			}
-			else {				
-				showDialog("오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.", null);
-				return;
-			}
-		},
-		error: function(jqXHR, text, error){
-			showDialog("죄송합니다, 일시적인 오류가 발생하였습니다. 다시 시도 부탁드립니다.", null);
-			hideLoader();
-		}
-	});
-}
-
-function showPrivacyDialog() {	
-	$('#modal_title_content').text("APLY 개인정보처리방침");
-    $('#modal_body_content').load("privacy_for_email.html");
-    $('#modal-3').modal('show');
-}
-
-function validateNumber(event) {
-    var key = window.event ? event.keyCode : event.which;
-    if (event.keyCode === 8 || event.keyCode === 46) {
-        return true;
-    } else if ( key < 48 || key > 57 ) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-function setSubmitHandler(form_p_id) {
-	var form_id = "#" + form_p_id;
-
-	$(form_id + "_send").on("click", function(e) {
-		e.preventDefault();
-
-		if (appSent == true) {
-			if (confirm('이미 전송한 내용이 있습니다. 다시 진행 하시겠습니까?')) {	}
-			else {
-			  return;
-			}
-		}
-
-		showLoader();
-
-		sendApplicationData(form_id);				
-	});
-
-	$('[name^=form_phone]').keypress(validateNumber);
 }
 
 function ajaxRequest(data, callback, errorcallback) {		
@@ -379,35 +200,4 @@ function ajaxRequest(data, callback, errorcallback) {
             errorcallback(request, status, error);
         }
 	});
-}
-
-function GATAGM(event_name, category, label) {    
-    gtag(
-        'event', event_name, {
-        'event_category': category,
-        'event_label': label        
-    }
-    );
-}
-
-function showDialog(msg, callback) {
-	$('#askModalContent').text(msg);
-	$('#askModal').modal('show');
-
-	if (callback == null) return;
-
-	$('#askModalOKButton').off('click');
-	$('#askModalOKButton').click(function () {
-			$('#askModal').modal('hide');
-			callback();
-	});
-}
-
-
-function isSet(value) {
-    if (typeof (value) === 'number')
-        return true;
-    if (value == "" || value == null || value == "undefined" || value == undefined)
-        return false;
-    return true;
 }
